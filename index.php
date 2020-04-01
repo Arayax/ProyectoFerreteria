da0a0e8b9753936648476f2a0f34bc856f1c44aa
Johan <araya8025@gmail.com>
2020-04-01 00:27:22 -0600

    se crea el index y la galeria

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!doctype html>
+<html lang="en">
+ <?php include("Funciones/Header.php")?> 
+
+  <body>
+   <?php include("Funciones/menu.php")?> 
+
+<main role="main" class="container">
+<br>
+<br>
+<br>
+<br>
+  <div class="starter-template">
+    <h1>Bootstrap starter template</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+  </div>
+
+</main><!-- /.container -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script></body>
+</html>
